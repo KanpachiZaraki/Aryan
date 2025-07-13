f4290640f6d03e612ef1e4c16abc0e76dbc8484f
KanpachiZaraki <aryanforjs@gmail.com>
2025-07-13 21:23:29 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
+function fade(){
+    document.getElementById("Aryan").classList.add("fade-in");
+}
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry =>{
+        document.getElementById("ImageSection").classList.add("hide")
+    })
+})
+
+
+window.addEventListener("scroll", () => {
+  const section = document.getElementById("c");
+  const trigger = window.innerHeight - 100;
+  const top = section.getBoundingClientRect().top;
+
+  if (top < trigger) {
+    section.classList.add("show");
+  }
+});
+
 
